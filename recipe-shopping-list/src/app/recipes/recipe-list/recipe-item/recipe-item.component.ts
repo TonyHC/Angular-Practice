39375a455cd69b24a609ac2c8174bf4d5978259d
Tony Chao <tchao@mail.usf.edu>
2021-12-17 21:01:41 -0500

    Use input annotation to retrieve the desired recipe instead of using the recipe service get recipe by index

--- recipe-shopping-list/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ recipe-shopping-list/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,17 +7,13 @@ import { RecipeService } from '../../recipe.service';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input()
   recipe!: Recipe;
 
   @Input()
   index!: number;
 
-  constructor(private recipeService: RecipeService) {
-
-  }
-
   ngOnInit(): void {
-    this.recipe = this.recipeService.getRecipe(this.index);
   }
 
 }
